a3a64864dc54b8a38949a6086206c722a2f9b97b
aisarthak <aisarthak2@gmail.com>
2023-12-10 14:22:28 +0530

    000
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- src/components/components/Navbar.jsx
+++ src/components/components/Navbar.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import { Link } from 'react-router-dom'
+
+import {styles} from './styles'
+// import {navLink} from '../constants/index.js'
+import navLinks from './constants/index.js'
+import {logo,menu,close} from '../../assets'
+
 
 const Navbar = () => {
   return (
