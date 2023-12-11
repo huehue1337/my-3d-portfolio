e3ae0f5ab25455440ad2fe5ff3d01526be70d162
aisarthak <aisarthak2@gmail.com>
2023-12-11 17:34:16 +0530

    computer,progress bar, motion component
    copy pasted the computer and loader component and it started working what is the problem with my code . created motion div up and down button with framer motion ,

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,10 +21,29 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-2
            text-white-100`}>
             I am a full stack developer with a passion for building beautiful and functional websites.
-           </p>
+          </p>
         </div>
       </div>
-        <ComputersCanvas />
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 ">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1 '
+            />
+
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,32 +1,58 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { useGLTF, Preload, OrbitControls } from "@react-three/drei";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
-  // console.log(computer)
+
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} 
-      groundColor="black" />
-      <pointLight intensity={1} />
+      <hemisphereLight intensity={5} groundColor='white' />
       <spotLight
-      position={[-20,50,10]}
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={10}
+        castShadow
+        shadow-mapSize={1024}
       />
+      <pointLight intensity={2}  />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
 };
 
-
-
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop='demand'
@@ -35,15 +61,14 @@ const ComputersCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
+
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <mesh>
-          <Computers />
-        </mesh>
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
@@ -51,5 +76,4 @@ const ComputersCanvas = () => {
   );
 };
 
-
-export default Computers;
+export default ComputersCanvas;
