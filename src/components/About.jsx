a58f961470570145e369e59279c178347eb1a89b
aisarthak <aisarthak2@gmail.com>
2023-12-11 18:53:19 +0530

    about card
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
+// importing tilt
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+
+
+
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full '>
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 2)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card '
+      >
+        <div className=' bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col  ' onptions={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }} >
+          <img src={icon} alt={title} className='w-16 h-1/6 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center  ' > {title} </h3>
+        </div>
+      </motion.div>
+
+    </Tilt>
+  )
+}
+
+
+
+
+
+
+
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.heroHeadText} >Overview</h2>
+      </motion.div>
+
+
+      <motion.p
+        variants={fadeIn("", "", 0.01, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] '
+      >
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10 '>
+        {services.map((services, index) => (
+          <ServiceCard services={services} key={index} {...services} />
+        ))}
+      </div>
+    </>
   )
 }
 
---
+++ src/hoc/sectionwrapper.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const sectionwrapper = () => {
+  return (
+    <div>sectionwrapper</div>
+  )
+}
+
+export default sectionwrapper
